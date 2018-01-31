4f778f5a17ec1586223e134ce711973488e3dcc2
刘东旭 <chuyang009@163.com>
2018-02-01 01:07:53 +0800

    files

---
+++ ImageToVideo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageToVideo
+//
+//  Created by 刘东旭 on 2017/9/3.
+//  Copyright © 2017年 刘东旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageToVideo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageToVideo
+//
+//  Created by 刘东旭 on 2017/9/3.
+//  Copyright © 2017年 刘东旭. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
